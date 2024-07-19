2f19009b633b6d67a9bba8d91d82fd2c96bed641
Phurithatvongsonthi <phurithat.name2546@gmail.com>
2024-07-19 16:06:38 +0700

    ตัวเลข

--- ep1/index.ts
+++ ep1/index.ts
@@ -5,3 +5,8 @@ let อยากกลับบ้าน : boolean = false
 console.log (firstname)
 console.log(y)
 console.log(อยากกลับบ้าน)
+const a : number = '1'
+const b: number = '4'
+console.log (a==b)
+console.log (a>b)
+console.log (a<=b)
