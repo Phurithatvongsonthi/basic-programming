b6da639121896ca5e0be83488c1bda7941a614b8
Phurithatvongsonthi <phurithat.name2546@gmail.com>
2024-07-23 14:20:11 +0700

    ตัวแปร

---
+++ ep.1/ep.1/index.ts
@@ -0,0 +1,15 @@
+const firstName : string = 'Phurithat'
+console.log(firstName)
+
+let Y : number = 10
+Y = 20
+console.log(Y)
+
+let iWantToGoHome: boolean = false 
+console.log(iWantToGoHome)
+
+const a: number = 10
+const b: number = 12
+console.log(a===b)
+console.log(a>b)
+console.log(a<=b)
