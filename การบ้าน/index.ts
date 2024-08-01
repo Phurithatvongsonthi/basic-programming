fff503c23c60ff6e403646d46b9f75264baed2c6
Phurithatvongsonthi <phurithat.name2546@gmail.com>
2024-08-02 01:48:28 +0700

    การบ้าน

--- การบ้าน/index.ts
+++ การบ้าน/index.ts
@@ -1,16 +1,16 @@
-function BMI (Height: number, Weight: number) {
-    let BMI : number (Weight / (Height * Height))
-    if(body< 18.50) {
-        return 'น้ำหนักน้อย,ผอม'
-    }else if (BMI >= 18.50 && BMI <= 22.90) {
-        return 'ปกติ'
-    }else if (BMI >= 23 && BMI <= 24.90) {
-        return 'ท้วม , โรคอ้วนระดับ 1'
-    }else if (BMI >= 25 && BMI <= 29.90) {
-        return 'อ้วน , โรคอ้วนระดับ 2'
-    }else if {
-        return 'อ้วนมาก , โรคระดับ 3'
-    }
+function BMI(Weight: number, Height: number){
+    let body: number = (Weight / (Height*Height))
+    
+    if(body < 18.5){
+        return 'ผอม'
+    } else if (body >= 18.50 && body <= 22.90 ){
+        return 'สมส่วน '
+    } else if (body >= 23 && body <= 24.90){
+        return 'เริ่มอ้วน '
+    } else if(body >= 25 && body <= 29.90){
+        return 'อ้วน'
+    }else if (body > 30){
+        return 'อ้วนมากไปแล้ว'
+    }return
 }
-
-console.log(BMI(180,1.78))
+console.log(BMI(100,1.90))
