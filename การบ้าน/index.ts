48e931b38ab08257600a7efbe841d6835da28a7b
Phurithatvongsonthi <phurithat.name2546@gmail.com>
2024-08-02 09:16:44 +0700

    การบ้าน

--- การบ้าน/index.ts
+++ การบ้าน/index.ts
@@ -2,15 +2,15 @@ function BMI(Weight: number, Height: number){
     let body: number = (Weight / (Height*Height))
     
     if(body < 18.5){
-        return 'ผอม'
+        return 'น้ำหนักน้อย/ผอม'
     } else if (body >= 18.50 && body <= 22.90 ){
-        return 'สมส่วน '
+        return 'ปกติ '
     } else if (body >= 23 && body <= 24.90){
-        return 'เริ่มอ้วน '
+        return 'ท้วม '
     } else if(body >= 25 && body <= 29.90){
         return 'อ้วน'
     }else if (body > 30){
-        return 'อ้วนมากไปแล้ว'
+        return 'อ้วนมากๆ'
     }return
 }
 console.log(BMI(100,1.90))
