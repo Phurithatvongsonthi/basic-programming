8613b56ca5e93cd1c1bddd4338636c214524b1f5
Phurithatvongsonthi <phurithat.name2546@gmail.com>
2024-09-06 00:22:48 +0700

    แนะนำตัว

---
+++ การบ้านแนะนำตัวเอง/index.ts
@@ -0,0 +1,26 @@
+const me = {
+    firstName: 'Abraham' ,
+    LastName : 'Convent',
+    age : 25,
+    phoneNumber : 0285456865,
+    sex : 'male',
+    addres : 'Spain',
+    city : 'Barcelona',
+    numBerCity : 850005,
+    street :  'girona',
+    numberStreet : 7,
+    mail : 'Abraham3045@gmail.com'
+}
+
+// ประวัติส่วนตัว
+
+// ชื่อ : Abraham
+// นามสกุล : Convent
+// อายุ : 25
+//เบอร์โทร : 0285456865
+//เพศ : ชาย
+//ที่อยู่ : Spain
+//เมือง : Barcelona
+//ถนน : girona
+//ถนนหมายเลข : 7
+//เมล :Abraham3045@gmail.com
